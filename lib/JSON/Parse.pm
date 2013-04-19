b3010c1f61c10d006cd4f1b4d67e15993057b1d2
Ben Bullock <bkb@cpan.org>
2013-04-19 11:38:01 +0000

    Import of BKB/JSON-Parse-0.19_03 from CPAN.
    gitpan-cpan-distribution: JSON-Parse
    gitpan-cpan-version:      0.19_03
    gitpan-cpan-path:         BKB/JSON-Parse-0.19_03.tar.gz
    gitpan-cpan-author:       BKB
    gitpan-cpan-maturity:     developer

--- lib/JSON/Parse.pm
+++ lib/JSON/Parse.pm
@@ -1,34 +1,36 @@
-=encoding UTF-8
-
-=head1 NAME
-
-Json3 - abstract here.
-
-=head1 SYNOPSIS
-
-    use Json3;
-
-=head1 DESCRIPTION
-
-=head1 FUNCTIONS
-
-=cut
 package JSON::Parse;
 require Exporter;
 @ISA = qw(Exporter);
-@EXPORT_OK = qw/parse_json/;
+@EXPORT_OK = qw/parse_json json_to_perl valid_json json_file_to_perl/;
 %EXPORT_TAGS = (
     all => \@EXPORT_OK,
 );
 use warnings;
 use strict;
 use Carp;
-our $VERSION = '0.19_02';
+our $VERSION = '0.19_03';
 require XSLoader;
 XSLoader::load (__PACKAGE__, $VERSION);
 
 our $true = 1;
 our $false = 0;
 our $null;
+
+sub json_to_perl
+{
+    goto &parse_json;
+}
+
+sub valid_json
+{
+    my ($json) = @_;
+    if (! $json) {
+	return undef;
+    }
+    eval {
+	parse_json (@_);
+    };
+    return ! $@;
+}
 
 1;
